a31821f29035aeca5bfaa88c4134d979b1da7173
Reda Makarem <makarem.reda@gmail.com>
2024-01-08 23:48:50 +0300

    Initial commit

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\CreateUserRequest;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function createUser(CreateUserRequest $request)
+    {
+        $user = User::create($request->only(['name', 'email', 'password']));
+        $user->profile()->create([
+            'country_id' => $request->country_id,
+            'mobile' => $request->mob,
+            'dob' => $request->dob,
+            'gender' => $request->gender,
+            'device_id' => $request->device_id
+        ]);
+        $user->profile->save();
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'message' => 'User created successfully',
+            'user' => $user,
+            'token' => $token
+        ], 201);
+    }
+    
+
+    public function loginUser()
+    {
+        $credentials = request(['email', 'password']);
+        if (!auth()->attempt($credentials)) {
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+        $user = User::where('email', $credentials['email'])->first();
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'message' => 'User logged in successfully',
+            'user' => $user,
+            'token' => $token
+        ], 200);
+    }
+}
---
+++ database/migrations/2023_11_19_111023_create_profiles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string("mobile");
+            $table->date("dob");
+            $table->string("gender");
+            $table->string("device_id");
+            $table->foreignId("user_id")->constrained("users");
+            $table->foreignId("country_id")->constrained("countries");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
