22c8d203017e7f5747ebcf6109ee96b1fcbbd6f8
Reda Makarem <makarem.reda@gmail.com>
2024-01-10 03:41:55 +0300

    Add route for root endpoint

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,11 @@ use App\Http\Controllers\API\GoogleSignIn;
 |
 */
 
+
+Route::get('/', function () {
+    return response()->json(['message' => 'Hello World!']);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
