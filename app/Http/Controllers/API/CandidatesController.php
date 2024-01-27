730b1f21209730932dbfe487619d8a44d9814337
Reda Makarem <makarem.reda@gmail.com>
2024-01-28 00:33:25 +0300

    Add horoscope attribute to Candidate model and update CandidatesController

--- app/Http/Controllers/API/CandidatesController.php
+++ app/Http/Controllers/API/CandidatesController.php
@@ -9,7 +9,7 @@ class CandidatesController extends Controller
 {
     public function index()
     {
-        $candidates = auth()->user()->candidates;
+        $candidates = auth()->user()->candidates->each->append('horoscope');
         return response()->json([
             'message' => 'Candidates fetched successfully',
             'candidates' => $candidates
@@ -18,35 +18,33 @@ class CandidatesController extends Controller
 
 
     public function store(Request $request)
-{
-    $this->validate(request(), [
-        'name' => 'required',
-        'email' => 'required|email',
-        'mobile' => 'required',
-        'dob' => 'required|date',
-        'image' => 'sometimes|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-        
-    ]);
+    {
+        $this->validate(request(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'mobile' => 'required',
+            'dob' => 'required|date',
+            'image' => 'sometimes|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
 
-    $candidate = auth()->user()->candidates()->create([
-        'name' => request('name'),
-        'email' => request('email'),
-        'mobile' => request('mobile'),
-        'dob' => request('dob'),
-        
-    ]);
-    if ($request->hasFile('image')) {
-        $path = $request->file('image')->store('images', 'public');
-        $candidate->update(['candidate_image' => $path]);
+        ]);
+
+        $candidate = auth()->user()->candidates()->create([
+            'name' => request('name'),
+            'email' => request('email'),
+            'mobile' => request('mobile'),
+            'dob' => request('dob'),
+
+        ]);
+        if ($request->hasFile('image')) {
+            $path = $request->file('image')->store('images', 'public');
+            $candidate->update(['candidate_image' => $path]);
+        }
+
+        return response()->json([
+            'message' => 'Candidate created successfully',
+            'candidate' => $candidate
+        ], 201);
     }
 
-    return response()->json([
-        'message' => 'Candidate created successfully',
-        'candidate' => $candidate
-    ], 201);
+    
 }
-
-
-}
-
-
--- app/Models/Candidate.php
+++ app/Models/Candidate.php
@@ -20,4 +20,39 @@ class Candidate extends Model
     {
         return $this->belongsToMany(Criteria::class)->withPivot('response');
     }
+
+
+    public function getHoroscopeAttribute()
+    {
+        $date = $this->dob;
+        $month = date('m', strtotime($date));
+        $day = date('d', strtotime($date));
+
+    if (($month == 1 && $day <= 19) || ($month == 12 && $day >= 22)) {
+        return 'Capricorn';
+    } elseif (($month == 1 && $day >= 20) || ($month == 2 && $day <= 18)) {
+        return 'Aquarius';
+    } elseif (($month == 2 && $day >= 19) || ($month == 3 && $day <= 20)) {
+        return 'Pisces';
+    } elseif (($month == 3 && $day >= 21) || ($month == 4 && $day <= 19)) {
+        return 'Aries';
+    } elseif (($month == 4 && $day >= 20) || ($month == 5 && $day <= 20)) {
+        return 'Taurus';
+    } elseif (($month == 5 && $day >= 21) || ($month == 6 && $day <= 20)) {
+        return 'Gemini';
+    } elseif (($month == 6 && $day >= 21) || ($month == 7 && $day <= 22)) {
+        return 'Cancer';
+    } elseif (($month == 7 && $day >= 23) || ($month == 8 && $day <= 22)) {
+        return 'Leo';
+    } elseif (($month == 8 && $day >= 23) || ($month == 9 && $day <= 22)) {
+        return 'Virgo';
+    } elseif (($month == 9 && $day >= 23) || ($month == 10 && $day <= 22)) {
+        return 'Libra';
+    } elseif (($month == 10 && $day >= 23) || ($month == 11 && $day <= 21)) {
+        return 'Scorpio';
+    } elseif (($month == 11 && $day >= 22) || ($month == 12 && $day <= 21)) {
+        return 'Sagittarius';
+    }
+        
+    }
 }
